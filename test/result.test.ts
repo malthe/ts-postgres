cdd09ad2125782d43869e568bdbaa94e5090e85b
Matthieu Sieben <matthieu.sieben@gmail.com>
2019-12-06 22:25:19 +0100

    Use text[] as test type

--- test/result.test.ts
+++ test/result.test.ts
@@ -123,7 +123,7 @@ describe('Result', () => {
 
     testWithClient('Null typed array', async (client) => {
         expect.assertions(1);
-        let row = await client.query('select null::uuid[] as value').one();
+        let row = await client.query('select null::text[] as value').one();
         expect(row.get('value')).toEqual(null);
     });
 });
