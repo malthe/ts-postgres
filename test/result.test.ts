7d4e2ac38fecefb15b7cefe26d8af75f2f692a4f
Malthe Borch <mborch@gmail.com>
2019-12-07 09:50:39 +0000

    Simplify test expectations

--- test/result.test.ts
+++ test/result.test.ts
@@ -67,28 +67,20 @@ describe('Result', () => {
         expect(row.get('bad')).toEqual(undefined);
     });
 
-    testWithClient('Multi dimentional value', async (client) => {
+    testWithClient('Parse array containing null', async (client) => {
         expect.assertions(1);
         let row = await client.query(
-            'select $1::text[][] as a', [[['a'],['b', 'c']]]
+            'select ARRAY[null::text] as a'
         ).one();
-        expect(row.get('a')).toEqual([['a'],['b', 'c']]);
+        expect(row.get('a')).toEqual([null]);
     });
 
-    testWithClient('Parsing multi-dimentional containing null', async (client) => {
+    testWithClient('Format array containing null value', async (client) => {
         expect.assertions(1);
         let row = await client.query(
-            'select ARRAY[ARRAY[null::text]] as a'
+            'select $1::text[] as a', [null]
         ).one();
-        expect(row.get('a')).toEqual([[null]]);
-    });
-
-    testWithClient('Formating multi-dimentional containing null value', async (client) => {
-        expect.assertions(1);
-        let row = await client.query(
-            'select $1::text[][] as a', [[[null]]]
-        ).one();
-        expect(row.get('a')).toEqual([[null]]);
+        expect(row.get('a')).toEqual([null]);
     });
 
     testWithClient('One', async (client) => {
